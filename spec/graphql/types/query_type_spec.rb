c1f8b4db8f0ecec6eb0d4da990404dc8ed39a143
Oscar Bernardini <bernardini687@gmail.com>
2019-09-04 15:10:51 +0200

    test for users query

--- spec/graphql/types/query_type_spec.rb
+++ spec/graphql/types/query_type_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe Types::QueryType do
     let!(:items) { create_pair(:item) }
 
     let(:query) do
-      %(query {
-        items {
-          title
+      <<~Q
+        query {
+          items {
+            title
+          }
         }
-      })
+      Q
     end
 
     subject(:result) do
@@ -19,6 +21,30 @@ RSpec.describe Types::QueryType do
     it 'returns all items' do
       expect(result.dig('data', 'items')).to match_array(
         items.map { |item| { 'title' => item.title } }
+      )
+    end
+  end
+
+  describe 'users' do
+    let!(:users) { create_pair(:user) }
+
+    let(:query) do
+      <<~Q
+        query {
+          users {
+            firstName
+          }
+        }
+      Q
+    end
+
+    subject(:result) do
+      MartianLibrarySchema.execute(query).as_json
+    end
+
+    it 'returns all users' do
+      expect(result.dig('data', 'users')).to match_array(
+        users.map { |user| { 'firstName' => user.first_name } }
       )
     end
   end
