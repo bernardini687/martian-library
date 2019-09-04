be69af6278b765ba4b2214ada5f4d928b34d02db
Oscar Bernardini <bernardini687@gmail.com>
2019-09-04 09:20:51 +0200

    add validations on models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, :image_url, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :items, dependent: :destroy
+
+  validates :first_name, :email, presence: true
+  validates :email, uniqueness: true
 end
