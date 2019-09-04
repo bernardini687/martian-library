8cb06c8b57674871083fb8b614efe6da8f554d9d
Oscar Bernardini <bernardini687@gmail.com>
2019-09-04 09:26:32 +0200

    craft seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'clearing records...'
+User.destroy_all
+
+puts 'adding users...'
+sophie = User.create!(
+  email: 'sophie@sophie.cat',
+  first_name: 'Sophie',
+  last_name: 'the cat'
+)
+
+margot = User.create!(
+  email: 'margot@margot.cat',
+  first_name: 'Margot',
+  last_name: 'the cat'
+)
+
+puts 'adding items...'
+Item.create!(
+  [
+    {
+      title: 'Martian Chronicles',
+      description: 'Cult book by Ray Bradbury',
+      user: sophie,
+      image_url: 'https://upload.wikimedia.org/wikipedia/en/4/45/The-Martian-Chronicles.jpg'
+    },
+    {
+      title: 'The Martian',
+      description: 'Novel by Andy Weir about an astronaut stranded on Mars trying to survive',
+      user: sophie,
+      image_url: 'https://upload.wikimedia.org/wikipedia/en/c/c3/The_Martian_2014.jpg'
+    },
+    {
+      title: 'Doom',
+      description: 'A group of Marines is sent to the red planet via an ancient ' \
+                   'Martian portal called the Ark to deal with an outbreak of a mutagenic virus',
+      user: margot,
+      image_url: 'https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg'
+    },
+    {
+      title: 'Mars Attacks!',
+      description: 'Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor',
+      user: margot,
+      image_url: 'https://upload.wikimedia.org/wikipedia/en/b/bd/Mars_attacks_ver1.jpg'
+    }
+  ]
+)
+
+puts 'done!'
